19d0230c4848a541fc6542120571be9388eb0e36
Ken Keller <ken.keller@hashicorp.com>
2023-09-06 19:58:56 -0500

    tf config

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_user" "secrets_sync" {
+  name = "vsult_secrets_sync"
+  path = "/"
+
+  tags = {
+    tag-key = "vault"
+  }
+}
+
+resource "aws_iam_access_key" "secrets_sync" {
+  user = aws_iam_user.secrets_sync.name
+}
+
+output "access_key" {
+   value = aws_iam_access_key.secrets_sync.id
+}
+
+output "secret_key" {
+   value = aws_iam_access_key.secrets_sync.secret
+   sensitive = true
+}
+
+data "aws_iam_policy_document" "secrets_sync_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = [ "secretsmanager:DescribeSecret",
+                "secretsmanager:GetSecretValue",
+                "secretsmanager:CreateSecret",
+                "secretsmanager:PutSecretValue",
+                "secretsmanager:UpdateSecret",
+                "secretsmanager:UpdateSecretVersionStage",
+                "secretsmanager:DeleteSecret",
+                "secretsmanager:RestoreSecret",
+                "secretsmanager:TagResource",
+                "secretsmanager:UntagResource"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "secrets_sync_policy" {
+  name   = "secrets_sync"
+  user   = aws_iam_user.secrets_sync.name
+  policy = data.aws_iam_policy_document.secrets_sync_policy.json
+}
